255931dad035436880c915d5934acb724c9ca133
muriithivyke-code <muriithivyke@gmail.com>
2026-06-12 11:06:03 +0300

    Add Header and Footer components; update App to include Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
+
+
 function Header (){
     return (
         <>
+                <header>
+                    <img src="#" alt="Logo icon" />
+                    <h1>Echocoach.ai</h1>
+                </header>
         </>
     )
 }
+
+export default Header;
