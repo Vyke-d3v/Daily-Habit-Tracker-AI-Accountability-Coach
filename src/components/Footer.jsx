3edc00cf525953b7eb25e114c5b31f760583c308
muriithivyke-code <muriithivyke@gmail.com>
2026-06-13 23:36:50 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,16 @@ function Footer (){
 
     return (
         <>
-            <div>
-                
-            </div>
+            <footer>
+                <div>
+                    <h3>Quick links</h3>
+                    <div className="contactlinks">
+                        <a href="tel:0739589044">Journal on Whatsapp</a>
+                        <a href="mailto:echocoach@gmail.com">Mail us</a>
+                    </div>
+                </div>
+                <p>&copy; Echocoach.ai daily habit tracker 2026</p>
+            </footer>
         </>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-
+import logo from '../assets/full-logo.png'
 
 function Header (){
     return (
         <>
                 <header>
-                    <img src="#" alt="Logo icon" />
+                    <img src={logo} alt="Logo icon" />
                     <h1>Echocoach.ai</h1>
+                    <a href="habits">HABITS </a>
                 </header>
         </>
     )
