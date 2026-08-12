37add06ccd4a97c7ca671624fc53a91d0b7e5204
muriithivyke-code <muriithivyke@gmail.com>
2026-08-12 11:13:01 +0300

    update Habit.jsx

--- src/pages/User/Habit.jsx
+++ src/pages/User/Habit.jsx
@@ -1,8 +1,10 @@
 function Habits (){
     return(
         <>
-        </>
-    )
+        <h2>Welcome to your habit Tracker.</h2>
+        <description>Enter your daily habits here.</description>
+        <input type="button" value="add-habit"></input></>
+    );
 }
 
 export default Habits;
