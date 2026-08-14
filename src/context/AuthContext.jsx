2856df243f845778a688bf550a7eb3e2103cdebc
muriithivyke-code <muriithivyke@gmail.com>
2026-08-15 00:51:15 +0300

    update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,1 +1,24 @@
-import { createContext,useContext,useEffect } from "react";
+import { createContext,useContext,useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import {auth} from "../services/firebase"
+
+const AuthContext = createContext();
+export function AuthProvider({children}){
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] =useState(true);
+
+    useEffect(()=>{
+        const unsub=onAuthStateChanged(auth,(u)=>{
+            setUser(u)
+            setLoading(false);
+        });
+        return unsub
+    }, []);
+    return(
+        <AuthContext.Provider value={{user,loading}}>
+            {!loading  && children}
+        </AuthContext.Provider>
+    )
+    }
+}
