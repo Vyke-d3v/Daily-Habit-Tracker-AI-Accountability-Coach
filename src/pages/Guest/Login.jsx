56d392514df84c0d66c248d7b0abce55d31ed6d2
muriithivyke-code <muriithivyke@gmail.com>
2026-08-29 15:13:24 +0300

    fix: update Login component to use styled components and add form structure

--- src/pages/Guest/Login.jsx
+++ src/pages/Guest/Login.jsx
@@ -1,3 +1,9 @@
+// "use client";
+
+// import {Check} from "gravity-ui/icons";
+import {Button, Form, Input, Label} from "@heroui/react";
+// import {Button, Description, FieldError, Form, Input, Label, Textfield} from "@heroui/react";
+// import {Button, Description, FieldError, Form, Input, Label, Textfield} from "@heroui/react";
 import { Link } from "react-router-dom";
 function Login (){
     return(
@@ -7,15 +13,19 @@ function Login (){
                 <p>Start tracking your daily Habits.</p>
             </div>
             <div className="Login-form">
-                <form action="Register">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="user-name" id="user-name" placeholder="Eg. Vyke Muriithi"/>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="user-email" id="user-email" placeholder="Eg. muriithivyke@gmail.com" />
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="user-password" id="user-password" placeholder="Eg. echocoach@2026" />
+                <Form action="Register">
+                    <Label htmlFor="name">Name</Label>
+                    <Input type="text" name="user-name" id="user-name" placeholder="Eg. Vyke Muriithi"/>
+                    <Label htmlFor="email">Email</Label>
+                    <Input type="email" name="user-email" id="user-email" placeholder="Eg. muriithivyke@gmail.com" />
+                    <Label htmlFor="password">Password</Label>
+                    <Input type="password" name="user-password" id="user-password" placeholder="Eg. echocoach@2026" />
 
-                </form>
+                    <Button>
+                        <button type="reset" className="bg-blue-500 text-white px-4 py-2 rounded" variant>Log In</button>
+                    </Button>
+
+                </Form>
 
             </div>
             <div className="loginAlt">
