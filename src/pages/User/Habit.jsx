6a7124d9bae98a5fcbd4b70da2aa3a3fd14a94f2
muriithivyke-code <muriithivyke@gmail.com>
2026-09-10 21:18:50 +0300

    fix: implement habit movement functionality in Habit component

--- src/pages/User/Habit.jsx
+++ src/pages/User/Habit.jsx
@@ -17,14 +17,29 @@ function addHabit(){
 }
 
 function deleteHabit(index){
-
+    const updatedHabit = habit.filter((_, i) => i !== index);
+    setHabit(updatedHabit);
 }
 
 function moveHabitup(index){
+    const updatedHabit = [...habit];
+    if (index > 0) {
+        const temp = updatedHabit[index - 1];
+        updatedHabit[index - 1] = updatedHabit[index];
+        updatedHabit[index] = temp;
+        setHabit(updatedHabit);
+    }
     
 }
 
 function moveHabitDown(index){
+    const updatedHabit = [...habit];
+    if (index < updatedHabit.length - 1) {
+        const temp = updatedHabit[index + 1];
+        updatedHabit[index + 1] = updatedHabit[index];
+        updatedHabit[index] = temp;
+        setHabit(updatedHabit);
+    }
 
 }
 
